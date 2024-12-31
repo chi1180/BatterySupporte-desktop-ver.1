fed434e5c92d6de8925e5d0770b982a202754073
Chi_1 <143685104+chi1180@users.noreply.github.com>
2024-12-31 15:54:54 +0900

    may be...ok

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const {app, BrowserWindow, Tray, Menu, screen} = require('electron/main')
+const path = require('node:path')
+
+const ICON_PATH = path.join(__dirname, "./favicon.ico");
+
+let win;
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
+  win = new BrowserWindow({
+    width: width,
+    height: height,
+    icon: ICON_PATH,
+  });
+  win.setMenu(null);
+
+  win.on("close", (event) => {
+    event.preventDefault();
+    win.hide();
   });
 
-  mainWindow.loadFile('index.html');
+  win.loadURL("https://battery-supporter-web-ver-2.vercel.app/#dashboard")
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Quit",
+      click: () => {
+        process.exit(0);
+      }
+    }
+  ]);
+
+  const tray = new Tray(ICON_PATH);
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    win.show();
+  });
+
+  tray.setToolTip("BatterySupporter");
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    win.hidden();
+  }
 });
+
+function getPropertyOnLocalStorage(property) {
+  return JSON.parse(localStorage.getItem("settings-data"))[property];
+}
+
